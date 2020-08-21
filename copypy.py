45ce1a7dac9026b466ed8fc0816c064035892653
Emmanuel idehen <23498282+emmanuelidehen@users.noreply.github.com>
2020-08-21 12:50:49 -0500

    Add files via upload

---
+++ copypy.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+data = np.array([1, 3, 5, 2, 3, 7, 8, 4, 10, 0, 6, 7, 3, 0, 3, 0, 5, 7, 10, 1, 4, 9, 3])
+
+# TODO: Complete this function by having the function return the average value of our dataset.
+def compute_mean(dataset):
+    """ Main function that calculates the average value across our data. """
+
+data.copy()
+data = np.array([2,4,6])
+#compute_mean(data)
+for x in data :
+    print (x)
